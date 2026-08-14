da624caec8153ee0d4f5abbd3e21cac51e414cac
Shakil Ahmmed <36266736+sXakil@users.noreply.github.com>
2026-08-14 06:30:24 +0600

    fix: add web support for cookie management by using in-memory storage instead of persistent file storage

--- lib/utils/dio.dart
+++ lib/utils/dio.dart
@@ -2,6 +2,7 @@ import 'package:cookie_jar/cookie_jar.dart';
 import 'package:dio/dio.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:dio_cookie_manager/dio_cookie_manager.dart';
+import 'package:flutter/foundation.dart';
 
 class DioUtil {
   static late CookieJar _cookieJar;
@@ -9,11 +10,18 @@ class DioUtil {
   static Dio get dio => _dio;
 
   static Future<void> init() async {
-    final dir = await getApplicationSupportDirectory();
-    _cookieJar = PersistCookieJar(
-      storage: FileStorage('${dir.path}/.cookies/'),
-    );
     _dio = Dio(BaseOptions(validateStatus: (status) => true));
-    _dio.interceptors.add(CookieManager(_cookieJar));
+
+    if (kIsWeb) {
+      _cookieJar = CookieJar();
+      // Browsers handle cookies automatically; 
+      // we initialize an in-memory CookieJar to avoid null errors.
+    } else {
+      final dir = await getApplicationSupportDirectory();
+      _cookieJar = PersistCookieJar(
+        storage: FileStorage('${dir.path}/.cookies/'),
+      );
+      _dio.interceptors.add(CookieManager(_cookieJar));
+    }
   }
 }
